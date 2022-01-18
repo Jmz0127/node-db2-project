cd6a60198faa768c93dc80f861a77ded56ce41e9
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-18 17:20:17 -0500

    added vin validator and seeds

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,38 @@
 // STRETCH
+const cars = [
+	{
+		vin: '1111111111111',
+		make: 'tesla',
+		model: 'model Y',
+		mileage: 215000,
+		title: 'clean',
+		transmission: 'auto'
+	},
+	{
+		vin: '1111111111112',
+		make: 'tesla',
+		model: 'model 3',
+		mileage: 215000,
+		title: 'used'
+	},
+	{
+		vin: '1111111111113',
+		make: 'tesla',
+		model: 'model X',
+		mileage: 215000
+	}
+];
+
+exports.seed = function (knex) {
+	return knex('cars')
+		.truncate()
+		.then(() => {
+			return knex('cars').insert(cars);
+		});
+};
+
+//same as above but with async await instead
+// exports.seed = async function(knex) {
+//     await knex('cars').truncate()
+//     await knex('cars').insert(cars)
+//     }
