066b14350591c8a881a34dea8f137f0d4bc8696d
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-18 19:54:14 -0500

    updated server file

--- api/server.js
+++ api/server.js
@@ -1,7 +1,23 @@
-const express = require("express")
+const express = require('express');
+const res = require('express/lib/response');
 
-const server = express()
+const server = express();
+
+server.use(express.json()); //parsing for json
+
+//error catch all
+server.use('*', (req, res, next) => {
+	next({ status: 404, message: 'not found' });
+});
+
+//error handling middleware for the above
+server.use((err, req, res, next) => {
+	//eslint-disable-line
+	res.status(err.status || 500).json({
+		message: err.message
+	});
+});
 
 // DO YOUR MAGIC
 
-module.exports = server
+module.exports = server;
