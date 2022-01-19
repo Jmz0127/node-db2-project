c592ab0b2dcbdae1b8e3755a3383f4f2149de593
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-18 19:17:31 -0500

    reran database with some tweaks to migrate table

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,17 +1,17 @@
 exports.up = async function (knex) {
-  // DO YOUR MAGIC - if youre not doing async remove async and replace await with return!!
-  await knex.schema.createTable('cars', table => {
-    table.increments();
-    table.string('vin', 17).unique().notNullable();
-    table.string('make', 128).notNullable();
-    table.string('model' 128).notNullable();
-    table.numeric('milage').unsigned().notNullable(); //unsigned means the number can't be in the negatives
-    table.string('title', 128); //for non required columns you can add a .defaultTo('string here')
-    table.string('transmission', 128);
-  })
+	// DO YOUR MAGIC - if youre not doing async remove async and replace await with return!!
+	await knex.schema.createTable('cars', (table) => {
+		table.increments();
+		table.string('vin', 17).unique().notNullable();
+		table.string('make', 128).notNullable();
+		table.string('model', 128).notNullable();
+		table.integer('milage').unsigned().notNullable(); //unsigned means the number can't be in the negatives
+		table.string('title', 128); //for non required columns you can add a .defaultTo('string here')
+		table.string('transmission', 128);
+	});
 };
 
 exports.down = async function (knex) {
-  // DO YOUR MAGIC
-  await knex.schema.dropTableIfExists('cars')
+	// DO YOUR MAGIC
+	await knex.schema.dropTableIfExists('cars');
 };
